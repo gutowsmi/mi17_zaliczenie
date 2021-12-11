78202807aff34191f30be67e588d134eb51e149f
Michał Gutowski <michal.gutowski2002@gmail.com>
2021-12-11 14:21:27 +0100

    jsc done

--- classes/jsc.php
+++ classes/jsc.php
@@ -1,6 +1,31 @@
 <?php
 require_once "capital.php";
 class Jsc extends Capital{
-    
+    private int $stockprice;
+    private int $stocks;
+    function __construct($name,$founder,$capital,$StockExchange){
+        parent::__construct($name,$founder,$capital,$StockExchange,$stockprice,$stocks);
+        $this->stockprice = $stockprice;
+        $this->stocks = $stocks;
+    }
+    public function SetStockPrice($stockprice){
+        $this->stockprice = $stockprice;
+    }
+    public function GetStockPrice(){
+        return $this->stockprice;
+    }
+    public function SetStock($stocks){
+        $this->stocks = $stocks;
+    }
+    public function GetStock(){
+        return $this->stocks;
+    }
+    public function __toString() {
+        $worth = $this->GetWorth();
+        return "Type of Company: Capital\nName:$this->name S.A\nFounder: $this->founder\nCapital: $this->captial\nThis company is worth $worth";
+    }
+    public function GetWorth(){
+        return $this->stockprice*$this->stocks;
+    }
 }
 ?>
