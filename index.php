2a694f67787b950e1d2cba7be9557207f44e0fa2
Michał Gutowski <michal.gutowski2002@gmail.com>
2021-12-11 19:36:41 +0100

    do not ask, i hate it too

--- index.php
+++ index.php
@@ -11,27 +11,65 @@ foreach (glob("classes/*.php") as $filename)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Klasy</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<table>
-    <tr>
-        <th>id</th>
-        <th>name</th>
-        <th>imgSrc</th>
-        <th>title</th>
-        <th>level</th>
-        <th>parentId</th>
-    </tr>
+<div id="container">
+    <div class="holder">
+    <table>
+        <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>imgSrc</th>
+            <th>title</th>
+            <th>level</th>
+            <th>parentId</th>
+        </tr>
+    <?php
+    $data = [];
+    $db = mysqli_connect("localhost","root","","zaliczenie");
+    $query = "SELECT * FROM classes";
+    $result = mysqli_query($db,$query);
+    $counter = 0;;
+    $data = [];
+    while($row = mysqli_fetch_array($result)){
+        echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td><td>$row[5]</td></tr>";
+        array_push($data,"<img src='img/placeholder.jpg' title='$row[3]' id='$counter'>");
+        $counter++;
+    }
+    ?>
+    </table>
+    </div>
+    <div class="holder">
+        <h1>Dziedziczenie</h1>
+        <?php
+        echo "<div>";
+            echo "<div>";
+            echo $data[0];
+            echo "</div>";
+                echo "<div id='diagram-1'>";
+                echo $data[2];
+                echo $data[1];
+                echo $data[3];
+                echo "</div>";
+            echo "<div id='diagram-2'>";
+                echo "<div id='diagram-3'>";
+                echo $data[6];
+                echo $data[7];
+                echo "</div>";
+                echo "<div id='diagram-4'>";
+                echo $data[4];
+                echo $data[5];
+                echo "</div>";
+            echo "</div>";
+        echo "</div>";
+    ?>
+    </div>
+</div>
 <?php
-$db = mysqli_connect("localhost","root","","zaliczenie");
-$query = "SELECT * FROM classes";
-$result = mysqli_query($db,$query);
-while($row = mysqli_fetch_array($result)){
+    $db->close();
 
-}
 ?>
-</table>
 </body>
 </html>
 
