637ca18b52cd708fa872518e3fc2d8889d5a8ca6
Michał Gutowski <michal.gutowski2002@gmail.com>
2021-12-11 15:17:57 +0100

    all classes done

--- classes/general.php
+++ classes/general.php
@@ -1,6 +1,24 @@
 <?php
 require_once "personal.php";
 class General extends Personal{
-    
+    private String $DateOfDisabling;
+    function __construct($name,$founder,$capital,$HQLocation,$DateOfDisabling){
+        parent::__construct($name,$founder,$capital,$HQLocation);
+        $this->DateOfDisabling = $DateOfDisabling;
+    }
+    public function SetDateOD($DateOfDisabling){
+        $this->DateOfDisabling = $DateOfDisabling;
+    }
+    public function GetDateOD(){
+        return $this->DateOfDisabling;
+    }
+    public function DaysTillDisable(){
+        $now = date_create();
+        $diff = date_diff($now,$this->DateOfDisabling)/60/60/24
+        return "It is $diff days till disband of this company";
+    }
+    public function __toString() {
+        return "Name:$this->name\nFounder: $this->founder\nCapital: $this->captial\nHQ Location: $this->HQLocation\nDate of disband: $this->DateOfDisabling";
+    }
 }
 ?>
--- classes/partnership.php
+++ classes/partnership.php
@@ -1,6 +1,22 @@
 <?php
 require_once "personal.php";
 class Partnership extends Personal{
-    
+    private String $profession;
+    function __construct($name,$founder,$capital,$HQLocation,$profession){
+        parent::__construct($name,$founder,$capital,$HQLocation);
+        $this->profession = $profession;
+    }
+    public function SetProfession($profession){
+        $this->profession = $profession;
+    }
+    public function GetProfession(){
+        return $this->profession;
+    }
+    public function __toString() {
+        return "Name:$this->name\nFounder: $this->founder\nCapital: $this->captial\nHQ Location: $this->HQLocation\nThis company provides $this->profession services";
+    }
+    public function LookForWork(){
+        print("We are ardevtising our $this->profession services")
+    }
 }
 ?>
