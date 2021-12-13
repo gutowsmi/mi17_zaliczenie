a4bac52989b1a8de268f597920eb796d40e62511
Michał Gutowski <michal.gutowski2002@gmail.com>
2021-12-13 16:35:31 +0100

    all done

--- index.php
+++ index.php
@@ -11,11 +11,25 @@ function EchoForm($fieldid){
     <input type='submit' value='Zapisz' name='submit'>
     </form></div>";
 }
+function printTree($print_index, $connection) {
+    $query = "SELECT * FROM classes WHERE parentId = $print_index";
+    $res = mysqli_query($connection,$query);
+    while($row = mysqli_fetch_assoc($res)) {
+        $form = '"'."./forms/".$row['name'].".php".'"';
+        $uploadform = EchoForm($row['id']);
+        $img = $row['imgSrc'];
+        $title = $row['title'];
+        echo "<div class='col'><div><img src=$img title=$title onclick='document.location = $form'>$uploadform</div><div class='row'>";
+            printTree($row['id'], $connection);
+        echo "</div></div>";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Klasy</title>
@@ -34,18 +48,11 @@ function EchoForm($fieldid){
             <th>parentId</th>
         </tr>
     <?php
-    $data = [];
     $db = mysqli_connect("localhost","root","","zaliczenie");
     $query = "SELECT * FROM classes";
     $result = mysqli_query($db,$query);
-    $counter = 0;;
-    $data = [];
     while($row = mysqli_fetch_array($result)){
         echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td><td>$row[5]</td></tr>";
-        $form = '"'."./forms/$row[1].php".'"';
-        $uploadform = EchoForm($row[0]);
-        array_push($data,"<div><img src='$row[2]' title='$row[3]' id='$counter' onclick='document.location = $form'>$uploadform</div>");
-        $counter++;
     }
     ?>
     </table>
@@ -53,26 +60,7 @@ function EchoForm($fieldid){
     <div class="holder">
         <h1>Dziedziczenie</h1>
         <?php
-        echo "<div>";
-            echo "<div>";
-            echo $data[0];
-            echo "</div>";
-                echo "<div id='diagram-1'>";
-                echo $data[2];
-                echo $data[1];
-                echo $data[3];
-                echo "</div>";
-            echo "<div id='diagram-2'>";
-                echo "<div id='diagram-3'>";
-                echo $data[6];
-                echo $data[7];
-                echo "</div>";
-                echo "<div id='diagram-4'>";
-                echo $data[4];
-                echo $data[5];
-                echo "</div>";
-            echo "</div>";
-        echo "</div>";
+        printTree(0, $db);
     ?>
     </div>
 </div>
